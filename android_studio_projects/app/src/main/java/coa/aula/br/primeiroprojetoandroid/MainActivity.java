50110321aa170200dd6640028b96301a16042ae2
Jacobo Daniel <jacobo.daniel.coa@gmail.com>
2018-10-08 00:24:45 -0300

    interface start

---
+++ android_studio_projects/app/src/main/java/coa/aula/br/primeiroprojetoandroid/MainActivity.java
@@ -0,0 +1,15 @@
+
+
+package coa.aula.br.primeiroprojetoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
